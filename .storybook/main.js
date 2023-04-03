bfcbc5696e7a7fe501c898941e52772c58be377b
ryosuke-horie <ryosuke03140@gmial.com>
2023-04-03 23:05:25 +0900

    storybook can use node 16.x and set up

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,32 @@
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
+const path = require('path')
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: [
+    '../src/**/*.stories.mdx',
+    '../src/**/*.stories.@(js|jsx|ts|tsx)'
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-postcss',
   ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
-  }
+  staticDirs: ['public'],
+  babel: async options => ({
+    ...options,
+    plugins: [
+      '@babel/plugin-proposal-class-properties',
+      '@babel/plugin-proposal-private-methods',
+      '@babel/plugin-proposal-private-property-in-object',
+    ],
+  }),
+  webpackFinal: async (config) => {
+    config.resolve.plugins = [
+      new TsconfigPathsPlugin({
+        configFile: path.resolve(__dirname, '../tsconfig.json')
+      }),
+    ];
+
+    return config
+  },
 }
--- npm_command.md
+++ npm_command.md
@@ -10,4 +10,20 @@ npx sb init
 
 # プラグイン等のライブラリの導入
 npm i --save-dev @storybook/addon-postcss tsconfig-paths-webpack-plugin @babel/plugin-proposal-class-properties @babel/plugin-proposal-private-methods @babel/plugin-proposal-private-property-in-object tsconfig-paths-webpack-plugin @mdx-js/react
+
+npm install -g @storybook/cli
+npm uninstall @storybook/react webpack
+npm install @storybook/react webpack
+npm install typescript@4.3
+npm install react-docgen-typescript
+```
+
+# Storybookがnode18.xで動かないのでnvmを導入
+ https://github.com/coreybutler/nvm-windows/releases
+ setup.exeをインストール
+
+# nvmでバージョンを変更
+```
+nvm install 16.13.0
+nvm use 16.13.0
 ```
