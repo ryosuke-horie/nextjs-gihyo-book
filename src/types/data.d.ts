a25b7360ea8ece1295780c7f2913e7fd911df24c
ryosuke-horie <ryosuke03140@gmial.com>
2023-04-07 18:23:48 +0900

    アプリケーションで利用されるデータの型を作成

---
+++ src/types/data.d.ts
@@ -0,0 +1,33 @@
+import { Description } from '@mui/icons-material'
+
+// 商品のカテゴリ
+export type Category = 'shoes' | 'clothes' | 'book'
+// 商品の状態
+export type Condition = 'new' | 'used'
+
+// ユーザー
+export type User = {
+  id: number
+  username: string
+  displayName: string
+  email: string
+  profileImageUrl: string
+  description: string
+}
+
+// 商品
+export type Product = {
+  id: number
+  category: Category
+  title: string
+  description: string
+  imageUrls: string
+  blurDataUrl: string
+  price: number
+  condition: Condition
+  owner: User
+}
+
+export type Context = {
+  apiRootUrl: string
+}
